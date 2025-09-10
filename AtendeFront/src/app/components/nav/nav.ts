1ef85e482e7fb2e37ddad60df3dae7c34cb29d73
Matheus.Lima <limamatheus0010@gmail.com>
2025-09-09 22:41:40 -0300

    Adicionar projeto Angular sem subpasta

---
+++ AtendeFront/src/app/components/nav/nav.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  imports: [],
+  templateUrl: './nav.html',
+  styleUrls: ['./nav.css']
+})
+export class Nav {
+
+}
