2c6f8bb65d9665315b6250f91e91647a3f55e27b
Matheus.Lima <limamatheus0010@gmail.com>
2025-09-14 21:46:08 -0300

    Feat: Adição do menu toggle

--- AtendeFront/src/app/app.routes.ts
+++ AtendeFront/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Nav } from './components/nav/nav';
 
+
 export const routes: Routes = [{
     
-    path: '', component: Nav
+    path: '', component: Nav,
+    
 
 }
 ];
--- AtendeFront/src/app/components/nav/nav.ts
+++ AtendeFront/src/app/components/nav/nav.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDrawer, MatSidenavModule } from "@angular/material/sidenav";
+import { RouterLink } from "@angular/router";
+import { MatListModule } from '@angular/material/list';
+import { MatToolbar } from "@angular/material/toolbar";
 
+
+/**
+ * @title Menu positioning
+ */
 @Component({
-  selector: 'app-nav',
-  imports: [],
-  templateUrl: './nav.html',
-  styleUrls: ['./nav.css']
+  selector: 'nav',
+  templateUrl: 'nav.html',
+  imports: [MatButtonModule, MatMenuModule, MatDrawer, RouterLink, MatSidenavModule, MatListModule, MatToolbar],
 })
-export class Nav {
-
-}
+export class Nav {}
