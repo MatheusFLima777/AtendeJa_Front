92cd31a3f9e91d09e0a95043d9903f0f216cae99
Matheus.Lima <limamatheus0010@gmail.com>
2025-09-18 12:51:47 -0300

    Feat: Criação do componente Header

--- AtendeFront/src/app/app.routes.ts
+++ AtendeFront/src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { Nav } from './components/nav/nav';
 import { Home } from './components/home/home';
 
 
+
 export const routes: Routes = [{
-    
-    path: '',
-    component: Nav,
-    children: [
-      { path: 'home', component: Home }, 
-      { path: '', redirectTo: 'home', pathMatch: 'full' }
-    
-    ]
-}
-];
+  path: '',
+  component: Nav,
+  children: [
+    { path: 'home', component: Home }, 
+    { path: 'ticket', children: [] },
+    { path: 'agentes', children: [] },
+    { path: 'funcionarios', children: [] },
+    { path: 'logout', children: [] },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+  ]
+}];
 
