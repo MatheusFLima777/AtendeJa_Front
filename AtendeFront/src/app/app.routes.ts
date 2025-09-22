883f2653d2f321fd7330a3d316a4fa28bef71927
Matheus.Lima <limamatheus0010@gmail.com>
2025-09-22 13:20:15 -0300

    Feat: Adição da rota do agente

--- AtendeFront/src/app/app.routes.ts
+++ AtendeFront/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Nav } from './components/nav/nav';
 import { Home } from './components/home/home';
+import { TecnicoList } from './components/tecnico/tecnico-list/tecnico-list';
 
 
 
@@ -10,7 +11,10 @@ export const routes: Routes = [{
   children: [
     { path: 'home', component: Home }, 
     { path: 'ticket', children: [] },
-    { path: 'agentes', children: [] },
+    { path: 'agentes', 
+            children: [
+            {path: '', component: TecnicoList },
+            ]},
     { path: 'funcionarios', children: [] },
     { path: 'logout', children: [] },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
