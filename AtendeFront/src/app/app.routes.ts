16a3f01fb1533229263628d7056e18ecf5e226f1
Matheus.Lima <limamatheus0010@gmail.com>
2025-09-24 13:13:20 -0300

    Feat: Adição da rota de login

--- AtendeFront/src/app/app.routes.ts
+++ AtendeFront/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { Nav } from './components/nav/nav';
 import { Home } from './components/home/home';
 import { AgenteList } from './components/agente/agente-list/agente-list';
+import { Login } from './components/login/login';
 
 
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {path: 'login',component: Login},
+  {
   path: '',
   component: Nav,
   children: [
