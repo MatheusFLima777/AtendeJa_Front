c4f0fa18f7ec3574b55d3464d2b667893517632d
Matheus.Lima <limamatheus0010@gmail.com>
2025-09-30 14:29:16 -0300

    Feat: Adicao de validação no formulario

--- AtendeFront/src/app/components/login/login.ts
+++ AtendeFront/src/app/components/login/login.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { Credenciais } from '../../models/credenciais';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
 
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  };
+
+  constructor() {}
+
+  // Retorna true se o formulário for válido
+  validaCampos(form: NgForm): boolean {
+    console.log(form.valid ? 'Credenciais:' : 'Formulário inválido', this.creds);
+    return !!form.valid; // converte null em false
+  }
+
+  // Ação do login
+  login(form: NgForm) {
+    if (form.valid) {
+      console.log('Login realizado com sucesso:', this.creds);
+      // Aqui você pode chamar seu serviço de autenticação
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
