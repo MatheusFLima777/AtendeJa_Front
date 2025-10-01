d08fe89981aae76f5847026bdd8faa02680f9acd
Matheus.Lima <limamatheus0010@gmail.com>
2025-10-01 18:51:35 -0300

    Feat: Adicao da pasta config + api.config

--- AtendeFront/src/app/services/auth.ts
+++ AtendeFront/src/app/services/auth.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Credenciais } from '../models/credenciais';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
+
+  constructor(private http: HttpClient){}
+
+  authenticate(creds: Credenciais){
+    return this.http.post()
+  }
   
 }
